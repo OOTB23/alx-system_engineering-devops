cea4cea4cce621d746f860456692f2da53fe24f0
OOTB <osamaquotp@gmail.com>
2023-08-10 20:08:40 -0700

    fix printed message and betty standard

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: p rints the size of various types on the computer it is compiled on
+ * Description: prints the size of various types on the compiling  computer
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,10 +11,11 @@ int main(void)
 	long l;
 	long long ll;
 	float f;
-	printf("size of char: %lu \n", (unsigned long)sizeof (c));
-	printf("size of int: %lu \n", (unsigned long)sizeof (i));
-	printf("size of long int: %lu \n", (unsigned long)sizeof (l));
-	printf("size of long long int: %lu \n", (unsigned long)sizeof (ll));
-	printf("size of long float: %lu \n", (unsigned long)sizeof (f));
+
+	printf("size of char: %lu byte(s) \n", (unsigned long)sizeof(c));
+	printf("size of int: %lu byte(s) \n", (unsigned long)sizeof(i));
+	printf("size of long int: %lu byte(s) \n", (unsigned long)sizeof(l));
+	printf("size of long long int: %lu byte(s) \n", (unsigned long)sizeof(ll));
+	printf("size of long float: %lu byte(s) \n", (unsigned long)sizeof(f));
 	return (0);
 }
