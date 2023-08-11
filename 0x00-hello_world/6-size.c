82488816e20ddcc920ec0a8019eb74c2b2b4ba2d
OOTB <osamaquotp@gmail.com>
2023-08-10 20:13:37 -0700

    fix printing message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf("size of char: %lu byte(s) \n", (unsigned long)sizeof(c));
-	printf("size of int: %lu byte(s) \n", (unsigned long)sizeof(i));
-	printf("size of long int: %lu byte(s) \n", (unsigned long)sizeof(l));
-	printf("size of long long int: %lu byte(s) \n", (unsigned long)sizeof(ll));
-	printf("size of long float: %lu byte(s) \n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s) \n", (unsigned long)sizeof(ll));
+	printf("Size of a long float: %lu byte(s) \n", (unsigned long)sizeof(f));
 	return (0);
 }
