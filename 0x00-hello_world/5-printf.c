d57c0ad3d043850426c29a67a70b52efdca2b78a
OOTB <osamaquotp@gmail.com>
2023-08-10 19:35:25 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: printing to StdOut
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Programming is like building a multilingual puzzle\n");
+return (0);
+}
