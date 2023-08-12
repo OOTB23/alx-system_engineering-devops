33b393022c3efbd9908f24d89a83e0e76d5dfe83
OOTB <osamaquotp@gmail.com>
2023-08-11 19:37:40 -0700

    fix betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,6 +8,7 @@
  * Description - indicate number sign
  * return - (0) on sucess
  */
+
 int main(void)
 {
 	int n;
@@ -17,16 +18,15 @@ int main(void)
 	/* your code goes there */
 	if (n<0)
 	{
-	    printf("%d is negative\n",n);
+	printf("%d is negative\n",n);
 	}
-	
 	else if (n>0)
 	{
-	    printf("%d is positive\n",n);
+	printf("%d is positive\n",n);
 	}
 	else
 	{ 
-	    printf("%d is zero\n",n);
+	printf("%d is zero\n",n);
 	}
 	return (0);
 }
