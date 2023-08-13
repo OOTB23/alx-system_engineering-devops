cbd31f9fc10a8a46a85c8cdfbcdc812b2fc3d458
OOTB <osamaquotp@gmail.com>
2023-08-13 01:20:51 -0700

    script print alhapbit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * describtion: function that print alphabit
+ *
+ * Return: (0) Always success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+return (0);
+
+}
