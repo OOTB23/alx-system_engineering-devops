4d540b9178a79092ffacf2fa460bf88be03c26ba
OOTB <osamaquotp@gmail.com>
2023-08-13 00:32:16 -0700

    last digit script using if statment

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - entery point
+ *
+ * description: function taht print last digit for rndm number
+ *
+ * Return:(0) Always success
+ */
+
 int main(void)
 {
 	int n;
@@ -10,6 +19,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	printf("%d n is ,n);
+	printf("Last digit of %d is", n);
+	if ((n % 10) > 5)
+	{
+		printf(" %d and is greater than 5", (n % 10));
+	}
+	else if ((n % 10) == 0)
+	{
+		printf(" %d and is 0", (n % 10));
+	}
+	else
+	{
+		printf(" %d and is less than 6 and not 0", (n % 10));
+	}
 	return (0);
 }
