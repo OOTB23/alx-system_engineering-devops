2cc73e75f43cbd99267e9eea7d2054f13be2d3a1
OOTB <osamaquotp@gmail.com>
2023-08-13 17:01:05 -0700

    script priny alphabest in lower then upercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * describtion: function that print alphabit in lower then upper case
+ *
+ * Return: (0) Always success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}	
+
+	putchar('\n');
+
+return (0);
+
+}
