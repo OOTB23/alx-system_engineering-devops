25a996b536235129b425643a682bc0e09a0b45c2
OOTB <osamaquotp@gmail.com>
2023-08-13 18:05:45 -0700

    print hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * describtion: function that print hexa numbers
+ *
+ * Return: (0) Always success
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 97 ; c <= 102 ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+return (0);
+
+}
