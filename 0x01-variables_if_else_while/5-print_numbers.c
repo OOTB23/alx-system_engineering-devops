16075c154c9cc51e3a276d63f6a5f37959e989fb
OOTB <osamaquotp@gmail.com>
2023-08-13 17:31:22 -0700

    count from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * describtion: count from 0 to 10
+ *
+ * Return: (0) Always success
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9 ; c++)
+	{
+		printf("%d", c);
+	}
+
+	putchar('\n');
+
+return (0);
+
+}
