e508d40e521359634b1580ecc29d0ae6a34cb2ce
OOTB <osamaquotp@gmail.com>
2023-08-13 17:44:26 -0700

    count from 0to9 characters

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ *
+ * describtion: count from 0 to 10
+ *
+ * Return: (0) Always success
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48 ; c <= 57 ; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+return (0);
+
+}
