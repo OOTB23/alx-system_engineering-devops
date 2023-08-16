60172c95504b6baa0e082358479c8df1530fcd41
OOTB <osamaquotp@gmail.com>
2023-08-16 15:31:57 -0700

    function call abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs- function give absolute value
+ * @n: The number to give its absolute value
+ * Return: (int r)- absolute value
+ *
+ */
+int _abs(int n)
+{
+
+	int r;
+
+	r = abs(n);
+
+	return (r);
+}
