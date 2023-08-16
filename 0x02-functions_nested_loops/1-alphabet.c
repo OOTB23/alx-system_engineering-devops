41fb721e532b734fd314bdd4867858210bf452d2
OOTB <osamaquotp@gmail.com>
2023-08-16 09:32:31 -0700

    fix betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,15 @@
  * @c: The character to print
  *
  * Return: nothing.
- * 
+ *
  */
 void print_alphabet(void)
 {
 	char ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{            
+	{
 		_putchar(ch);
- 	}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
