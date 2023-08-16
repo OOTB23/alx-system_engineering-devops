54d817d0053d4ac7da4e3f54f14ce5d85572aece
OOTB <osamaquotp@gmail.com>
2023-08-15 19:44:38 -0700

    fucnction call

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: invoke function that write single character
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	char s = "_putchar";
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
