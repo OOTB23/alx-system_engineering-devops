65d4ad1175ef8e7950fb3baab07cc445ec6ea0ed
OOTB <osamaquotp@gmail.com>
2023-08-16 08:47:29 -0700

    add function print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet - writes the alphabet using _putchar function
+ * @c: The character to print
+ *
+ * Return: 0 always success.
+ * 
+ */
+void print_alphabet(void);
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{            
+		_putchar(ch);
+ 	}
+		_putchar('\n');
+
+
+	return (0);
+}
