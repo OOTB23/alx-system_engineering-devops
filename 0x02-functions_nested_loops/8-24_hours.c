3a0f55702f3d8aefd54707da89298e301cd68303
OOTB <osamaquotp@gmail.com>
2023-08-16 19:23:17 -0700

    print time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer- print every minute
+ *
+ * Return: nothing
+ *
+ */
+
+void jack_bauer(void)
+{
+	int d1, d2, d3, d4 = 0;
+
+	for (; d4 <= 2; d4++)
+	{
+		for (d3 = 0; d3 <= 3; d3++)
+		{
+			for (d2 = 0; d2 <= 9; d2++)
+			{
+				for (d1 = 0; d1 <= 9; d1++)
+				{
+					_putchar(d4 + 48);
+					_putchar(d3 + 48);
+					_putchar(58);
+					_putchar(d2 + 48);
+					_putchar(d1 + 48);
+				}
+			}
+		}
+	}
+}
