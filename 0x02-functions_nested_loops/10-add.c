89363ccbfba57c56e8c99ae2399886c1f08ee485
OOTB <osamaquotp@gmail.com>
2023-08-16 18:34:31 -0700

    add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign- function indicate positive or negative number
+ * @a: The first number to be added
+ * @b: The second number to be added
+ * Return: (s)- sum of a b
+ *
+ */
+int add(int a,int b);
+{
+
+	int s;
+
+	s = a + b;
+
+	return (s);
+}
