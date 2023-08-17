c0cf4d3c6892a24d1b87ef9c2e2cd14d419a9ce9
OOTB <osamaquotp@gmail.com>
2023-08-17 14:40:13 -0700

    fix comment add to variable declaration

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	int i;
+/*	int i;*/
 
 	printf("Infinite loop incoming :(\n");
 /**
@@ -19,6 +19,6 @@ int main(void)
  *	}
  */
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
