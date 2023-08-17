af5053b1d2b39921f1f78f799d7819cc37b6c1ee
OOTB <osamaquotp@gmail.com>
2023-08-17 15:51:22 -0700

    fix equation code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,26 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-    if (a > b && a > c)
-    {
-	    largest = a;
-    }
-    else if (b > a && b > c)
-    {
-	    largest = b;
-    }
-    else
-    {
-	    largest = c;
-    }
+	if ((a == b) && (a > c))
+	{
+		largest = a;
+	}
+	else if ((a == c) && (a < b))
+	}
+		largest = b;
+	}
+	else if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
