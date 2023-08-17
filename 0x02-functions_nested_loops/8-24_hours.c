a399b7edb5e85f25c667a68cf7fcc955e42521eb
OOTB <osamaquotp@gmail.com>
2023-08-16 19:47:20 -0700

    conunters code fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,18 +12,25 @@ void jack_bauer(void)
 
 	for (; d4 <= 2; d4++)
 	{
-		for (d3 = 0; d3 <= 3; d3++)
+		for (d3 = 0; d3 <= 9; d3++)
 		{
-			for (d2 = 0; d2 <= 9; d2++)
+			if ((d4 == 2) && (d3 == 4))
 			{
-				for (d1 = 0; d1 <= 9; d1++)
+				break;
+			}
+			else
+			{
+				for (d2 = 0; d2 <= 5; d2++)
 				{
+					for (d1 = 0; d1 <= 9; d1++)
+					{
 					_putchar(d4 + 48);
 					_putchar(d3 + 48);
 					_putchar(58);
 					_putchar(d2 + 48);
 					_putchar(d1 + 48);
 					_putchar('\n');
+					}
 				}
 			}
 		}
