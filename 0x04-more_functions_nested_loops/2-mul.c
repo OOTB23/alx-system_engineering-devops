f4e6f8148466e6f0c1ce59cc3adaabd6d8eaa3f8
OOTB <osamaquotp@gmail.com>
2023-08-17 19:37:30 -0700

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul- function to give multiply of two digits
+ * @a: The first number to be multiplied
+ * @b: The second number to be multiplied
+ * Return: (s)- multiply of a b
+ *
+ */
+int mul(int a, int b)
+{
+
+	int s;
+
+	s = a * b;
+
+	return (s);
+}
