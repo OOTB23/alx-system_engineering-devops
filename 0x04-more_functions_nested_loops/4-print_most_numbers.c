d6e5c256431d646f79abc6e404c7dae4a73b2ba4
OOTB <osamaquotp@gmail.com>
2023-08-17 19:58:17 -0700

    print no 2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - writes the number 0-9 but not 2 or 4
+ *
+ * describtion: function that print alphabit
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		if (ch == 2 || ch == 4)
+		{
+		}
+		else
+		{
+			putchar(ch + 48);
+		}
+	}
+	putchar('\n');
+
+return (0);
+
+}
