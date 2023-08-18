5f0aaa27b74bf54f6c72f63f4aa61d8bb3f98772
OOTB <osamaquotp@gmail.com>
2023-08-17 20:57:38 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line- print line in terminal
+ * @n: number of - to form the line
+ *
+ * Return: nothing
+ *
+ */
+void print_line(int n)
+{
+	
+	while (n > 0)
+	{
+		_putchar(95);
+		n--;
+	}
+	_putchar('\n');
+}
+	
