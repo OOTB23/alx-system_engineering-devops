3685f824d8e45f77d84644e7dcf3592d183b790c
OOTB <osamaquotp@gmail.com>
2023-08-17 19:42:36 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - writes the alphabet
+ *
+ * Return: nothing.
+ *
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		_putchar(0+48);
+	}
+	_putchar('\n');
+}
