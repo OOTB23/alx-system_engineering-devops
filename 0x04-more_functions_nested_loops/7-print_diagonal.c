77480bce2fa1ca302cf23e46c03e5f046e49a2dc
OOTB <osamaquotp@gmail.com>
2023-08-18 19:57:29 -0700

    code fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,19 @@
  */
 void print_diagonal(int n)
 {
+	int x;
 
 	while (n > 0)
 	{
+		x = n;
 		_putchar(92);
 		_putchar('\n');
-		_putchar(32);
+		while (x > 1)
+		{
+			_puchar(32);
+			x--
+		}
+
 		n--;
 	}
 	_putchar('\n');
