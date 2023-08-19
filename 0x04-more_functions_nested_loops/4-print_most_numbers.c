2973b5e949250b6fc62732467ee24a8e19648ad6
OOTB <osamaquotp@gmail.com>
2023-08-18 18:36:29 -0700

    add again 4-print numbers file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,28 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers - writes the number 0-9 but not 2 or 4
+ * print_most_nuumbers - writes the number 0-9 but 2,4
  *
- * describtion: function that print alphabit
+ * Return: nothing.
  *
- * Return: nothing
  */
-
 void print_most_numbers(void)
 {
 	int ch;
 
 	for (ch = 0; ch <= 9; ch++)
 	{
-		if (ch == 2 || ch == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(ch + 48);
-		}
+		_putchar(ch + 48);
 	}
-	putchar('\n');
-
+	_putchar('\n');
 }
