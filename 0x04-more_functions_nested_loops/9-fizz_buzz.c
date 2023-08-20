5723bf04cf01a0337ca5a2172c2c8df95dfd0d1a
OOTB <osamaquotp@gmail.com>
2023-08-19 18:13:41 -0700

    space at end got fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,26 @@ int main(void)
 
 	for (count = 1; count <= 100; count++)
 	{
-		if (count % 3 == 0 && count % 5 == 0)
+		if (count == 1)
 		{
-			printf("FizzBuzz ");
+			printf("%d", count);
+		}
+
+		else if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf(" FizzBuzz");
 		}
 		else if (count % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (count % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", count);
+			printf(" %d ", count);
 		}
 
 	}
