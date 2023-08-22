f2b4873461082149536091c8fd7af504e166924a
OOTB <osamaquotp@gmail.com>
2023-08-21 17:44:27 -0700

    Project for pointers and arrays

--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -1,40 +1,48 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * more_numbers - writes the alphabet 10 times
+ * print_triangle- print triangle
+ * @size: size of square
  *
- * Return: nothing.
+ * Return: nothing
  *
  */
-
 int main(void)
 {
-/*void more_numbers(void)
-{*/
 	int size;
-	scanf("%d",&size);
+	scanf("%d", &size);
 	int x = size;
-        int y = size;
+	int y = size;
+	int l = 1;
 
-        if (size <= 0)
-        {
-                putchar('\n');
-        }
-        else
-        {
-                while (y > 0)
-                {
-                        x = size;
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		while (y > 0)
+		{
+			x = size;
+			
 			while (x > 0)
-                        {
-                                putchar(35);
-                                x--;
-                        }
+			{
+				if (l >=  size)
+				{
+					putchar(35);
+				}
+				else
+				{
+					putchar(32);
+				}
+				x--;
+				l++;
+			}
 
-                        putchar('\n');
-                        y--;
-                }
-        }
-	return 0;
+			putchar('\n');
+			y--;
+		}
+
+	}
+	return (0);
 }
