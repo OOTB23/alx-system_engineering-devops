1586198484d442e064ff27f6a1e33ed04ecb6c94
OOTB <osamaquotp@gmail.com>
2023-08-22 19:39:17 -0700

    fix code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
 
 	for (count = 0; count < (n - 1); count++)
 	{
-		printf("%c, ", (a[count] + 48));
+		printf("%d, ", a[count]);
 	}
-	printf("%c\n", (a[count] + 48));
+	printf("%d\n", a[count]);
 }
