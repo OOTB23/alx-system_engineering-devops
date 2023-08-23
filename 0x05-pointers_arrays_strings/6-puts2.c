c9d736b995a67b3a2687bb394406e557c56db48b
OOTB <osamaquotp@gmail.com>
2023-08-22 18:50:47 -0700

    fix code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,14 +11,14 @@ void puts2(char *str)
 	int len = 0;
 	int count = 0;
 
-	while (*(s + len) != ('\0'))
+	while (*(str + len) != ('\0'))
 	{
 		len = len + 1;
 	}
 
 	while (count < len)
 	{
-		_putchar(*s + count);
+		_putchar(*str + count);
 		count = count + 2;
 	}
 }
