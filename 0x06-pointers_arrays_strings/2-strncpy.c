05c7ead0e4b7ba98ed33f614166de88ffbe9b495
OKOTB <osamaquotp@gmail.com>
2023-08-25 20:07:51 -0700

    2-source code

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy- copy two strings
+ *
+ * @dest: string to copy to
+ *
+ * @src: String to be opy from
+ *
+ *@n: most number of string bytes
+ *
+ * Return: string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c;
+
+	for (c = 0; (src[c] != '\0') && c < n; c++)
+	{
+		dest[c] = src[c];
+	}
+
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
