7ff6ca5ca3f8b864edf6e391f94b710c70e2e64c
OKOTB <osamaquotp@gmail.com>
2023-08-26 16:30:30 -0700

    6-source code

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+int issep(char c);
+/**
+ * cap_string- function to capitalize words
+ *
+ * @s: input string
+ *
+ * Return: string with case changed
+ */
+char *cap_string(char *s)
+{
+	int c = 0;
+	int sep = 1;
+
+	do {
+		if ((s[c] >= 97 && s[c] <= 122) && sep)
+		{
+			s[c] = s[c] - 32;
+			sep = 0;
+		}
+		else
+		{
+			sep = issep(s[c]);
+		}
+		c++;
+	} while (s[c] != '\0');
+
+	return (s);
+}
+
+int issep(char c)
+{
+	int i;
+	int sep;
+	char separr[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	for (i = 0; i < 13; i++)
+	{
+		if (c == separr[i])
+		{
+			sep = 1;
+			break;
+		}
+		else
+		{
+			sep = 0;
+		}
+	}
+	return (sep);
+}
