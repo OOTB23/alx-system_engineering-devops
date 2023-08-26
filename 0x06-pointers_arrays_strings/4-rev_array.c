2d836af2faf45e88f77385248a065bec396b65f7
OKOTB <osamaquotp@gmail.com>
2023-08-26 11:01:51 -0700

    4-source code

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array- function to reverse array order
+ *
+ * @a: array on int
+ *
+ * @n: number of array elements
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int c;
+	int tmp;
+
+	while (c < (n / 2))
+	{
+		tmp = a[n - 1 - c];
+		a[n - 1 - c] = a[c];
+		a[c] = tmp;
+	}
+}
