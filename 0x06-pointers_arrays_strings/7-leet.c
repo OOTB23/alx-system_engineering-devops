a91939370b1e4b939461b5f0148f4b1596f81675
OKOTB <osamaquotp@gmail.com>
2023-08-26 17:39:39 -0700

    7-source code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * leet- function to convert to leet tping
+ *
+ * @s: input string
+ *
+ * Return: string after change
+ */
+char *leet(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		if (s[c] == 97 || s[c] == 65)
+		{
+			s[c] = '4';
+		}
+		else if (s[c] == 101 || s[c] == 69)
+		{
+			s[c] = '3';
+		}
+		else if (s[c] == 111 || s[c] == 79)
+		{
+			s[c] = 'o';
+		}
+		else if (s[c] == 116 || s[c] == 84)
+		{
+			s[c] = '7';
+		}
+		else if (s[c] == 108 || s[c] == 76)
+		{
+			s[c] = '1';
+		}
+		c++;
+	}
+
+	return (s);
+}
