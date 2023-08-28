2c7162bb698ccca154d270948fc783a4fd2ef0ea
OKOTB <osamaquotp@gmail.com>
2023-08-28 16:30:38 -0700

    mian.h and 1-source code

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset- function to store in memory location
+ *
+ * @s: input path 
+ * @b: value to store
+ * @n: number of mem locations
+ *
+ * Return: mem path after sotre
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
