a44b81dc72b0a623ca54c8855fc4af36b1a7eaf7
OKOTB <osamaquotp@gmail.com>
2023-08-28 20:01:44 -0700

    8-source code

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums-  function that prints the sum of the two diagonals
+ *
+ * @a: input array
+ * @size: array size
+ *
+ * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int d1 = 0;
+	int d2 = 0;
+
+
+	for (i = 0; i < size; i++)
+	{
+		d1 = d1 + a[i];
+		d2 = a[size - i - 1];
+		a = a + size;
+	}
+	printf("%d, ", d1);
+	printf("%d\n", d2);
+}
