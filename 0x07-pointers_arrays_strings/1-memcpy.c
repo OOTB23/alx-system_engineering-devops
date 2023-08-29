5f11a70cf32c9286db3473a508ad07fe6f9cd776
OKOTB <osamaquotp@gmail.com>
2023-08-28 17:02:10 -0700

    1-source code

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy- function to copy from mem location to another
+ *
+ * @src: input path
+ * @dest: output path
+ * @n: number of store bytes
+ *
+ * Return: output path
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		src[i] = dest[i];
+		n--;
+	}
+
+	return (dest);
+}
