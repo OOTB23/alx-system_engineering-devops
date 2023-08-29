5b018dcf84f8a148f91a44b9570ff282948271f5
OKOTB <osamaquotp@gmail.com>
2023-08-28 18:44:43 -0700

    3-source code

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn- function to fetch substring from a string
+ *
+ * @s: input string
+ * @accept: substring to look for
+ *
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int c;
+
+	for (c = 0; s[c] = '\0'; i++)
+	{
+		for (i = 0; accept[i] != s[c]; i++)
+		{
+			if (accept[i] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
