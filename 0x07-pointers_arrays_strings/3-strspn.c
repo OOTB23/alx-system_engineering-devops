e1d4daa3e119047a9f0d24c4a4b3ded09e299e79
OKOTB <osamaquotp@gmail.com>
2023-08-29 18:31:45 -0700

    fix code'

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i;
 	unsigned int c;
 
-	for (c = 0; s[c] != '\0'; i++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
 		for (i = 0; accept[i] != s[c]; i++)
 		{
